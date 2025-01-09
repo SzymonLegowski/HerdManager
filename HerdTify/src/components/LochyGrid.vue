7e7e2d8c3eeaac306a9c81e8c3dcc107fe619aa0
Szymon <szymon@localhost.localdomain>
2025-01-09 20:31:00 +0100

    09_01_2025 Pierwszy form, jeszcze nieobsłużony(AddWydarzenie)

---
+++ HerdTify/src/components/LochyGrid.vue
@@ -0,0 +1,52 @@
+<template>
+    <div class="grid-container">
+      <v-btn
+        v-for="number in 100"
+        :key="number"
+        class="grid-item"
+        :disabled="!items.includes(number)"
+        @click="handleClick(number)"
+        outlined
+      >
+        {{ number }}
+      </v-btn>
+    </div>
+  </template>
+  
+  <script setup>
+  import { defineProps, defineEmits } from 'vue';
+  
+  const props = defineProps({
+    items: {
+      type: Array,
+      required: true
+    }
+  });
+  
+  const emit = defineEmits(['update:selectedLocha']);
+  
+  const handleClick = (number) => {
+    console.log(`Button ${number} clicked`);
+    emit('update:selectedLocha', number);
+  };
+  </script>
+  
+  <style scoped>
+  .grid-container {
+    display: grid;
+    grid-template-columns: repeat(10, 1fr);
+    gap: 5px;
+    position: absolute;
+    background-color: #101010;
+    transform: translate(100px, 5px);
+  }
+  
+  .grid-item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 40px;
+    min-width: 0;
+    min-height: 40px;
+    }
+  </style>
